ccd5901f224e83f2dc903405661942a1d0588cae
User <user@example.com>
2025-11-01 21:14:49 +0300

    удаление

--- apartment-booking/src/utils/index.js
+++
@@ -1,5 +0,0 @@
-// Вспомогательная функция для создания URL страниц
-export function createPageUrl(pageName) {
-  // Преобразуем название страницы в нижний регистр для URL
-  return `/${pageName.toLowerCase()}`;
-}
